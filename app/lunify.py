8324dc9819cba236445d1b6f3a8759a0fa89dcc8
Andrew Abbey <andymaclabs@gmail.com>
2026-02-05 00:24:21 +1100

    Refactor and comment code

--- app/lunify.py
+++ app/lunify.py
@@ -1,237 +1,206 @@
 #-------------------------------------------------------------------------------
-# File:        lunify.py
-# Author:      Andrew "Shabadoo" Abbey
-# Description: Merges lua project into single file.
-# Date:        03 Feb 2026
+# File:         lunify.py
+# Author:       Andrew "Shabadoo" Abbey
+# Description:  Merges lua project into single file.
+# Date:         03 Feb 2026
+# Updated:      04 Feb 2026
 #-------------------------------------------------------------------------------
 
 import os
 import re
 import shutil
+import sys
 
 #-------------------------------------------------------------------------------
-# Function:    get_file_paths
-# Description: Walks the src directory and finds paths for all lua files.
-# Params:      entry_path - Path to the src directory
-# Return:      file_paths - Dictionary of lua files and paths
+# Description:  Validates command line arguments.
+# Params:       args - The sys.argv object (command line arguments).
+# Return:       Valid arguements (Boolean).
 #-------------------------------------------------------------------------------
-def get_file_paths(entry_path):
-  file_paths = {}
-  for root, dirs, files in os.walk(entry_path):
+def validate_args(args):
+  arg_1_valid = True
+  arg_2_valid = True
+
+  # Validate arg_1 - src directory
+  arg_1 = args[1]
+  if not os.path.isdir(arg_1):
+    arg_1_valid = False
+    print(f"Error: {arg_1} is not a valid directory.")
+  
+  # Validate arg_2 - build directory
+  arg_2 = args[2]
+  if not os.path.isdir(arg_2):
+    arg_2_valid = False
+    print(f"Error: {arg_1} is not a valid directory.")
+
+  return arg_1_valid and arg_2_valid
+
+
+#-------------------------------------------------------------------------------
+# Description:  Walks the src directory and finds paths for all lua files.
+# Params:       src_dir - Path to the src directory.
+# Return:       paths - Dictionary of lua files and paths.
+#-------------------------------------------------------------------------------
+def get_file_paths(src_dir):
+  paths = {}
+
+  # Walk directory structure
+  for root, _, files in os.walk(src_dir):
+    # Iterate files
     for file in files:
-      if file[-4:] == ".lua":
-        file_paths[file[:-4]] = os.path.join(root, file)
-  return file_paths
+      # Get file extension
+      file_ext = file[-4:]
+      # Add to path dictionary if lua file
+      if file_ext == ".lua":
+        module_name = file[:-4]
+        paths[module_name] = os.path.join(root, file)
+
+  return paths
 
 #-------------------------------------------------------------------------------
-# Function:    get_module_tree
-# Description: Recursively builds a dependency tree for the project.
-# Params:      mod_name - Module name (excluding extension .lua)
-#              file_paths - Dictionary of file paths
-# Return:      mod_tree - Dependency tree for the project
+# Description:  Recursively builds a dependency list (in call order) for
+#               the project.
+# Params:       module_name - Module name (excluding extension .lua).
+#               file_paths - Dictionary of file paths.
+# Return:       list - Dependency list for the project.
 #-------------------------------------------------------------------------------
-def get_module_tree(mod_name, file_paths):
-  mod_tree = {mod_name: []}
+def get_dependency_list(module_name, file_paths, list = []):
 
-  file_path = file_paths[mod_name]
+  file_path = file_paths[module_name]
 
   try:
+    # Read src file
     with open(file_path, "r") as f:
       for line in f:
+        # Search for dependency statements
         match = re.search(r'require\(\"(.*?)\"\)', line)
         if match:
-          dep_name = match.group(1)
-          mod_tree[mod_name].append(get_module_tree(dep_name, file_paths))
+          dependency = match.group(1)
+          # Recursively search for dependencies
+          get_dependency_list(dependency, file_paths)
+          # Add to dependency list if not already added
+          if not dependency in list:
+            list.append(dependency)
   except:
     print(f"Error: The file '{file_path}' was not found!")
 
-  return mod_tree
-#-------------------------------------------------------------------------------
-# Function:    copy_to_tmp
-# Description: Copies a module to tmp directory.
-# Params:      file_path
-#              tmp_file_path
-#-------------------------------------------------------------------------------
-def copy_to_tmp(file_path, tmp_file_path):
-  if not os.path.isfile(tmp_file_path):
-
-    print(f"  - Copying: {file_path} -> {tmp_file_path}")
-
-    # Read src file into memory
-    with open(file_path, "r") as f:
-      print(f"    - Reading: {file_path}...", end="")
-      lines = list(f)
-    print("Done.")
-
-    # Write modified file to tmp directory
-    with open(tmp_file_path, "w") as f:
-      print(f"    - Writing: {tmp_file_path}...", end="")
-      f.write("-- lunify.end\n")
-      f.writelines(lines)
-    print("Done.")
-
-  else:
-    print(f"File exists: '{tmp_file_path}'")
+  return list
 
 #-------------------------------------------------------------------------------
-# Function:    wrap_module_lines
-# Description: Wraps a module in a function
-# Params:      lines - Array of lines read from module
+# Description:  Deletes a directory.
+# Params:       dir_path - The path to the directory.
+# Return:       None.
 #-------------------------------------------------------------------------------
-def get_module_as_function(mod_name):
-  tmp = "./test/build/tmp"
-  tmp_mod_file_path = f"{tmp}\\{mod_name}.lua"
-
-  print(f"      - Wrapping: {tmp_mod_file_path}")
-
-  with open(tmp_mod_file_path, "r") as f:
-    mod_lines = list(f)
-
-  new_lines = []
-
-  wrap = True
-
-  for line in mod_lines:
-    if "lunify.start" in line:
-      wrap = False
-      continue
-
-    if "lunify.end" in line:
-      new_lines.append("local module = function()\n")
-      wrap = True
-      continue
-
-    if wrap:
-      new_lines.append("\t" + line)
-    else:
-      new_lines.append(line)
-
-  new_lines.append(f"end\nlocal {mod_name} = module()\n-- lunify.end\n")
-
-  return new_lines
+def delete_directory(dir_path):
+  # Check if directory exists
+  if not os.path.isdir(dir_path):
+    print(f"Delete Directory: {dir_path} does not exist.")
+    return
+  try:
+    # Delete directory
+    shutil.rmtree(dir_path)
+  except Exception as e:
+    print(f"Delete Directory Error: {e}")
 
 #-------------------------------------------------------------------------------
-# Function:    merge_module
-# Description: 
-# Params:      file_name
-#              file_path
-# Return:      module_tree
+# Description:  Creates a directory.
+# Params:       dir_path - The path to the directory to be created.
+# Return:       None.
 #-------------------------------------------------------------------------------
-def merge_modules(mod_name, dep_tree, file_paths):
-  src_mod_file_path = file_paths[mod_name]
-  dest = "./test/build/tmp"
-  tmp_mod_file_path = f"{dest}/{mod_name}.lua"
+def create_directory(dir_path):
+  # Check if directory exists
+  if os.path.isdir(dir_path):
+    print(f"Create Directory: {dir_path} already exists.")
+    return
+  try:
+    # Create directory
+    os.mkdir(dir_path)
+  except Exception as e:
+    print(f"Create Directory Error: {e}")
 
-  # Create tmp directory
-  if not os.path.isdir(dest):
+#-------------------------------------------------------------------------------
+# Description:  Cleans (delete and recreate) a directory
+# Params:       dir_path - The path to the directory.
+# Return:       None.
+#-------------------------------------------------------------------------------
+def clean_directory(dir_path):
+  delete_directory(dir_path)
+  create_directory(dir_path)
+
+#-------------------------------------------------------------------------------
+# Description:  Builds the ouput file and writes to build directory.
+# Params:       dependency_list - List of project dependencies (in call order).
+#               file_paths - Dictionarey of project file paths.
+#               build_dir_path - Path to the build directory.
+# Return:       None.
+#-------------------------------------------------------------------------------
+def build(dependency_list, file_paths, build_dir_path):
+  w_lines = ["local lunify_module\n"]
+
+  # Get root file and add to module list
+  root = list(file_paths)[0]
+  dependency_list.append(root)
+  module_list = dependency_list
+
+  for module_name in module_list:
+
+    # Read module src file
     try:
-      os.mkdir(dest)
-    except:
-      print(f"Error: The directory '{dest}' already exists!")
+      with open(file_paths[module_name], "r") as f:
+        r_lines = list(f)
+    except Exception as e:
+      print(f"Build Error: {e}")
 
-  print(f"Merging: {mod_name} dependent modules...")
+    # Start function wrap
+    if not module_name == root:
+      w_lines.append("lunify_module = function()\n")
 
-  # Iterate over module dependencies
-  for dep_name in dep_tree:
-    print(f"  - Merging: {dep_name} -> {mod_name}")
-    # Check if module dependency is already in tmp directory
-    tmp_dep_file_path = f"{dest}\\{dep_name}.lua"
-    if not os.path.isfile(tmp_dep_file_path):
-      # Copy to tmp directory
-      copy_to_tmp(file_paths[dep_name], tmp_dep_file_path)
-    else:
-      print(f"  - File exists: '{tmp_dep_file_path}'")
+    # Write src module
+    for line in r_lines:
 
-  # Read through module file
-  # Write each line of module file to tmp/module
-  # On find require statement -> swap require for fucntion returning dependent module
-  # Write rest of module file.
+      # Skip blank lines
+      if line.strip() == "":
+        continue
+      
+      # Skip require statements
+      if re.search(r'require\(\"(.*?)\"\)', line):
+        continue
+      
+      # Write line (indented)
+      w_lines.append(("" if module_name == root else "  ") + line)
 
-    if os.path.isfile(tmp_mod_file_path):
-      src_mod_file_path = tmp_mod_file_path
-
-    # Read mod_file into list
-    with open(src_mod_file_path, "r") as f:
-      print(f"  - Reading: {src_mod_file_path}")
-      src_mod_lines = list(f)
-    
-    # Iterate through list and write into mod file.
-    with open(tmp_mod_file_path, "w") as f:
-      print(f"  - Writing: {tmp_mod_file_path}")
-      for line in src_mod_lines:
-        
-        if "lunify.end" in line:
-          continue
-
-        # Check if line is a require statement for the dependency
-        if f"require(\"{dep_name}\")" in line:
-          
-          print(f"    - Call wrapping: {tmp_dep_file_path}")
-          
-          dep_mod_lines = get_module_as_function(dep_name)
-
-          print(f"    - Wrapped: {tmp_dep_file_path}")
-
-          print(f"    - Writing wrapped: {tmp_dep_file_path}")
-          f.writelines(dep_mod_lines)
-        else:
-          f.write(line)
-    print(f"  - Writing Complete: {tmp_dep_file_path}")
-
-  # try:
-  #   with open(file_path, "r") as f:
-  #     for line in f:
-  #       if line.strip() == "-- lunify_start":
-  #         print("Place holder")
-  # except:
-  #   print(f"Error: The file '{file_path}' was not found!")
+    # End function wrap
+    if not module_name == root:
+      w_lines.append(f"end\nlocal {module_name} = lunify_module()\n")
+  
+  # Write file
+  try:
+    with open(f"./{build_dir_path}/{root}.lunify.lua", "w") as f:
+      f.writelines(w_lines)
+  except Exception as e:
+    print(f"Build Error: {e}")
 
 #-------------------------------------------------------------------------------
-# Function:    merge_project
-# Description: 
-# Params:      mod_tree - Dependency tree for the project
-# Return:      None
+# Description:  The entry point of the applicaiton.
+# Params:       src_dir - The path to the src directory.
+#               build_dir_path - The path to the build directory.
+# Return:       None.
 #-------------------------------------------------------------------------------
-def merge_project(mod_tree, file_paths):
-  for mod_name, dep_array in mod_tree.items():
-    
-    # If no dependencies
-    if len(dep_array) == 0:
-      return
-    
-    # If dependencies, merge dependencies
-    for dep_tree in dep_array:
-      merge_project(dep_tree, file_paths)
-      # merge modules
-      merge_modules(mod_name, dep_tree, file_paths)
+def app(src_dir_path, build_dir_path):
+  # Get file paths for project files
+  file_paths = get_file_paths(src_dir_path)
 
-  return
+  root = list(file_paths)[0]
 
+  # Create ordered and flattened dependency list
+  dependency_list = get_dependency_list(root, file_paths)
 
-#-------------------------------------------------------------------------------
-# Function:    app
-# Description: The entry point of the applicaiton.
-# Params:      path - The path to the src directory
-# Return:      None
-#-------------------------------------------------------------------------------
-def app(path):
-  file_paths = get_file_paths(path)
-  print(file_paths)
-
-  mod_tree = get_module_tree("main", file_paths)
-  print(mod_tree)
-
-  merge_project(mod_tree, file_paths)
+  # Clean build directory
+  clean_directory(build_dir_path)
+  
+  # Build project
+  build(dependency_list, file_paths, build_dir_path)
 
 #-------------------------------------------------------------------------------
 if __name__ == "__main__":
- 
- build_dir = "./test/build"
-
- if os.path.isdir(build_dir + "/tmp"):
-   try:
-     shutil.rmtree(build_dir + "/tmp")
-   except OSError as e:
-     print(e)
-
- app('./test')
+ if validate_args(sys.argv):
+  app(sys.argv[1], sys.argv[2])
